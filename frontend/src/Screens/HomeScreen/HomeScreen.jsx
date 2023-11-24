8b1739dceaa40e95247a7a391f2b0aaf7b0cfbe4
Jake Samuels <samuels242@googlemail.com>
2023-11-24 22:18:49 +0000

    Import Hero and CardGrid

--- frontend/src/Screens/HomeScreen/HomeScreen.jsx
+++ frontend/src/Screens/HomeScreen/HomeScreen.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import Hero from "./../../Components/HomeScreen/Hero";
+import CardGrid from "../../Components/HomeScreen/CardGrid";
 
 const HomeScreen = () => {
-  return <div>HomeScreen</div>;
+  return (
+    <div className="screen homescreen">
+      <Hero />
+      <h2>Find your dream green location</h2>
+      <CardGrid />
+    </div>
+  );
 };
 
 export default HomeScreen;
