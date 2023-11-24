01701273c31eb3e963d2567cab269bbc5e1dafeb
Jake Samuels <samuels242@googlemail.com>
2023-11-24 23:58:20 +0000

    Link to Location screen

--- frontend/src/components/Shared/Card.jsx
+++ frontend/src/components/Shared/Card.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 const Card = ({ location }) => {
   return (
     <div className="location-card">
       <img src={location.img[0]} alt="" className="location-card__img" />
       <div className="location-card__text-content">
-        <p className="location-card__name">{location.name}</p>
+        <Link to={`/location/${location._id}`}>
+          <p className="location-card__name">{location.name}</p>
+        </Link>
         <p className="location-card__county">{location.county}</p>
         <p className="location-card__reviews">
           {location.reviews.length} Reviews
