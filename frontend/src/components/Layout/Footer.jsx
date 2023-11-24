32e352a5119d27b2e38d003683bf6d7924d42ce1
SimoneDessi <simonedessi1984@gmail.com>
2023-11-24 21:25:22 +0000

    will add the footer

---
+++ frontend/src/components/Layout/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+return (
+  <>
+  <div className="footer-container">
+    <a className="footer-link" href="">Home</a>
+    <a className="footer-link" href="">Profile</a>
+    <a className="footer-link" href="">Register</a>
+    <a className="footer-link" href="">Login</a>
+  </div>
+  
+  </>
+)
+}
