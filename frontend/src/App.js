d23b5b94ab0e2cf1abe5b73003702a25a98db051
SimoneDessi <simonedessi1984@gmail.com>
2023-11-24 20:30:49 +0000

    will add a navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import Hero from "./components/Hero.jsx";
+import Header from "./components/Layout/Header.jsx";
 
 const App = () => {
   return (
   <div>
+    <Header />
     <Hero />
   </div>
   )
---
+++ frontend/src/components/Layout/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <>
+    <nav className="navBar-container">
+      <a className="nav-link" href="frontend/src/components/screens/HomeScreen/HomeScreen.jsx">Home</a>
+      <a className="nav-link" href="frontend/src/components/screens/ProfileScreen/ProfileScreen.jsx">Profile</a>
+      <a className="nav-link" href="frontend/src/components/screens/AuthScreens/RegisterScreen.jsx">Register</a>
+      <a className="nav-link" href="frontend/src/components/screens/AuthScreens/LoginScreen.jsx">Login</a>
+    </nav>
+    
+    </>
+  )
+}
+export default Header
