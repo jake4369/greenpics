ffa79ec238eae64d1ced799721fc90a573f8d3a3
SimoneDessi <simonedessi1984@gmail.com>
2023-11-24 21:04:31 +0000

    added header on app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
-import Hero from "./components/Hero.jsx";
+import Hero from "./components/HomeScreen/Hero.jsx";
+import Header from "./components/Layout/Header.jsx";
 
 const App = () => {
   return (
   <div>
+    <Header />
     <Hero />
   </div>
   )
