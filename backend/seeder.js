718d51bbb9285ce439adaf20eebb10572450de8a
Jake Samuels <samuels242@googlemail.com>
2023-11-24 20:29:29 +0000

    Dev data seeder

---
+++ backend/seeder.js
@@ -0,0 +1,53 @@
+import dotenv from "dotenv";
+import colors from "colors";
+import users from "./data/users.js";
+import locations from "./data/locations.js";
+import User from "./models/userModel.js";
+import Location from "./models/locationModel.js";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+connectDB();
+
+const importData = async () => {
+  try {
+    await Location.deleteMany();
+    await User.deleteMany();
+
+    const createdUsers = await User.insertMany(users);
+
+    const adminUser = createdUsers[0]._id;
+
+    const sampleLocations = locations.map((location) => {
+      return { ...location, user: adminUser };
+    });
+
+    await Location.insertMany(sampleLocations);
+
+    console.log("Data Imported!".green.inverse);
+    process.exit();
+  } catch (error) {
+    console.error(`${error}`.red.inverse);
+    process.exit(1);
+  }
+};
+
+const destroyData = async () => {
+  try {
+    await Location.deleteMany();
+    await User.deleteMany();
+
+    console.log("Data Destroyed!".red.inverse);
+    process.exit();
+  } catch (error) {
+    console.error(`${error}`.red.inverse);
+    process.exit(1);
+  }
+};
+
+if (process.argv[2] === "-d") {
+  destroyData();
+} else {
+  importData();
+}
