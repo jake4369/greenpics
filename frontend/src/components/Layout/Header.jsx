94c0d4e9d478fb10248732e28da7f6b5b21f9b2b
Jake Samuels <samuels242@googlemail.com>
2023-11-25 12:00:18 +0000

    Add logout

--- frontend/src/components/Layout/Header.jsx
+++ frontend/src/components/Layout/Header.jsx
@@ -1,6 +1,27 @@
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useLogoutMutation } from "./../../slices/usersApiSlice";
+import { logout } from "./../../slices/authSlice";
 
 const Header = () => {
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [logoutApiCall] = useLogoutMutation();
+
+  const handleLogout = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <nav className="navBar-container">
@@ -13,9 +34,15 @@ const Header = () => {
         <Link to="/register" className="nav-link">
           Register
         </Link>
-        <Link to="/login" className="nav-link">
-          Login
-        </Link>
+        {userInfo ? (
+          <Link to="/" className="nav-link" onClick={handleLogout}>
+            Logout
+          </Link>
+        ) : (
+          <Link to="/login" className="nav-link">
+            Login
+          </Link>
+        )}
       </nav>
     </>
   );
