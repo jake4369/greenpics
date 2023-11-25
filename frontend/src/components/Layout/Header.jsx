bbcfd9ca003509f6985b26acaf2b0c046fa5b1e8
Jake Samuels <samuels242@googlemail.com>
2023-11-25 08:40:09 +0000

    Add Links to Nav

--- frontend/src/components/Layout/Header.jsx
+++ frontend/src/components/Layout/Header.jsx
@@ -1,14 +1,39 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
     <>
-    <nav className="navBar-container">
-      <a className="nav-link" href="frontend/src/components/screens/HomeScreen/HomeScreen.jsx">Home</a>
-      <a className="nav-link" href="frontend/src/components/screens/ProfileScreen/ProfileScreen.jsx">Profile</a>
-      <a className="nav-link" href="frontend/src/components/screens/AuthScreens/RegisterScreen.jsx">Register</a>
-      <a className="nav-link" href="frontend/src/components/screens/AuthScreens/LoginScreen.jsx">Login</a>
-    </nav>
-    
+      <nav className="navBar-container">
+        <Link
+          to="/"
+          className="nav-link"
+          href="frontend/src/components/screens/HomeScreen/HomeScreen.jsx"
+        >
+          Home
+        </Link>
+        <Link
+          to="/profile"
+          className="nav-link"
+          href="frontend/src/components/screens/ProfileScreen/ProfileScreen.jsx"
+        >
+          Profile
+        </Link>
+        <Link
+          to="/register"
+          className="nav-link"
+          href="frontend/src/components/screens/AuthScreens/RegisterScreen.jsx"
+        >
+          Register
+        </Link>
+        <Link
+          to="/login"
+          className="nav-link"
+          href="frontend/src/components/screens/AuthScreens/LoginScreen.jsx"
+        >
+          Login
+        </Link>
+      </nav>
     </>
-  )
-}
-export default Header
+  );
+};
+export default Header;
