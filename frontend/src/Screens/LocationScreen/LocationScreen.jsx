c951fffa7782a428c8ea59a3e1464cf7bfc8141c
Jake Samuels <samuels242@googlemail.com>
2023-11-25 00:01:15 +0000

    Add location details

---
+++ frontend/src/Screens/LocationScreen/LocationScreen.jsx
@@ -0,0 +1,44 @@
+import { useParams } from "react-router-dom";
+import { useGetLocationQuery } from "../../slices/locationsSlice";
+import { Link } from "react-router-dom";
+
+import { FaArrowLeft } from "react-icons/fa";
+
+import LocationDetails from "../../Components/LocationScreen/LocationDetails";
+import PublicInfo from "../../Components/LocationScreen/PublicInfo";
+
+const LocationScreen = () => {
+  const { id: locationId } = useParams();
+
+  const {
+    data: location,
+    isLoading: loadingLocation,
+    isError,
+  } = useGetLocationQuery(locationId);
+
+  return (
+    <div className="screen locationscreen">
+      {loadingLocation ? (
+        <p>Loading...</p>
+      ) : isError ? (
+        <p>Error...</p>
+      ) : (
+        <div className="grid-row-1">
+          <div className="grid-col-1">
+            <Link to="/" className="back-btn">
+              <FaArrowLeft className="back-btn__icon" /> Back
+            </Link>
+            <LocationDetails location={location} />
+            <PublicInfo location={location} />
+          </div>
+
+          <div className="grid-col-2"></div>
+        </div>
+      )}
+
+      <div className="grid-row-2">REVIEWS</div>
+    </div>
+  );
+};
+
+export default LocationScreen;
