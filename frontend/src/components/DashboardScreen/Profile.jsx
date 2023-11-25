343e9f892b623b089370fa26b138d58445843e58
Jake Samuels <samuels242@googlemail.com>
2023-11-25 12:29:50 +0000

    Fetch user info

--- frontend/src/components/DashboardScreen/Profile.jsx
+++ frontend/src/components/DashboardScreen/Profile.jsx
@@ -1,22 +1,39 @@
 import { useSelector } from "react-redux";
+import { useGetUserByIdQuery } from "../../slices/usersApiSlice";
 
 const Profile = () => {
   const { userInfo } = useSelector((state) => state.auth);
 
+  const { data: user, isLoading, isError } = useGetUserByIdQuery(userInfo._id);
+
+  console.log(user);
+
   return (
     <div className="profile">
-      <h1>My Profile</h1>
-      <img src="/images/profile.jpeg" alt="" className="profile__img" />
-      <p className="profile__username"></p>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : isError ? (
+        <p>Error...</p>
+      ) : (
+        <>
+          <h1>My Profile</h1>
+          <img src={user.profileImage} alt="" className="profile__img" />
+          <p className="profile__username">{user.username}</p>
 
-      <p className="profile__name">Jake Samuels</p>
-      <p className="profile__email">jake@email.com</p>
+          <p className="profile__name">{user.name}</p>
+          <p className="profile__email">{user.email}</p>
 
-      <h2>About Me</h2>
-      <p className="profile__bio">
-        My name is Jake, and I love to find beautiful and amazing places to
-        explore
-      </p>
+          <h2>About Me</h2>
+          {user.bio !== "" ? (
+            <p className="profile__bio">
+              My name is Jake, and I love to find beautiful and amazing places
+              to explore
+            </p>
+          ) : (
+            <p>Edit profile to add your bio</p>
+          )}
+        </>
+      )}
     </div>
   );
 };
