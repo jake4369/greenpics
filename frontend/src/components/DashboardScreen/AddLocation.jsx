db08fe94321a4880fd4a859827c932bfff47d51f
Jake Samuels <samuels242@googlemail.com>
2023-11-25 19:28:04 +0000

    AddLocation form

--- frontend/src/components/DashboardScreen/AddLocation.jsx
+++ frontend/src/components/DashboardScreen/AddLocation.jsx
@@ -1,4 +1,8 @@
 import { useState } from "react";
+import { useAddLocationMutation } from "./../../slices/locationsSlice";
+
+import Map from "../Shared/Map";
+
 const AddLocation = () => {
   const [name, setName] = useState("");
   const [county, setCounty] = useState("");
@@ -11,88 +15,139 @@ const AddLocation = () => {
   const [lng, setLng] = useState(-4.7913);
   const [lat, setLat] = useState(54.3222);
 
+  const [addLocation, { isLoading, isError }] = useAddLocationMutation();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = {
+      name,
+      county,
+      img,
+      description,
+      parking,
+      disabledAccess,
+      food,
+      toilets,
+      lng,
+      lat,
+      reviews: [],
+    };
+
+    try {
+      const response = await addLocation(data);
+
+      setName("");
+      setCounty("");
+      setImg("");
+      setDescription("");
+      setParking(false);
+      setDisabledAccess(false);
+      setFood(false);
+      setToilets(false);
+      setLng(-4.7913);
+      setLat(54.3222);
+    } catch (error) {
+      console.error("Error adding location:", error);
+    }
+  };
+
   return (
     <div className="addlocation">
       <h1>Add A Location</h1>
 
-      <form>
-        <label>Location</label>
-        <input
-          type="text"
-          placeholder="Name of location"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : isError ? (
+        <p>Error...</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <label>Location</label>
+          <input
+            type="text"
+            placeholder="Name of location"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
 
-        <label>County</label>
-        <input
-          type="text"
-          placeholder="Name of county"
-          value={county}
-          onChange={(e) => setCounty(e.target.value)}
-        />
+          <label>County</label>
+          <input
+            type="text"
+            placeholder="Name of county"
+            value={county}
+            onChange={(e) => setCounty(e.target.value)}
+          />
 
-        <label>Upload Image</label>
-        <input
-          type="text"
-          value={img}
-          onChange={(e) => setImg(e.target.value)}
-        />
+          <label>Upload Image</label>
+          <input
+            type="text"
+            value={img}
+            onChange={(e) => setImg(e.target.value)}
+          />
 
-        <label>Description</label>
-        <textarea
-          placeholder="Add a description"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        ></textarea>
+          <label>Description</label>
+          <textarea
+            placeholder="Add a description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          ></textarea>
 
-        <ul>
-          <li>
-            <label>
-              Parking
-              <input
-                type="checkbox"
-                value={parking}
-                onChange={(e) => setParking((checked) => !checked)}
-              />
-            </label>{" "}
-          </li>
-          <li>
-            <label>
-              Disabled Access{" "}
-              <input
-                type="checkbox"
-                value={disabledAccess}
-                onChange={(e) => setDisabledAccess((checked) => !checked)}
-              />
-            </label>
-          </li>
-          <li>
-            <label>
-              Food and Drink{" "}
-              <input
-                type="checkbox"
-                value={food}
-                onChange={(e) => setFood((checked) => !checked)}
-              />
-            </label>
-          </li>
-          <li>
-            <label>
-              Toilets{" "}
-              <input
-                type="checkbox"
-                value={toilets}
-                onChange={(e) => setToilets((checked) => !checked)}
-              />
-            </label>
-          </li>
-        </ul>
+          <ul>
+            <li>
+              <label>
+                Parking
+                <input
+                  type="checkbox"
+                  value={parking}
+                  onChange={(e) => setParking((checked) => !checked)}
+                  checked={parking}
+                />
+              </label>{" "}
+            </li>
+            <li>
+              <label>
+                Disabled Access{" "}
+                <input
+                  type="checkbox"
+                  value={disabledAccess}
+                  onChange={(e) => setDisabledAccess((checked) => !checked)}
+                  checked={disabledAccess}
+                />
+              </label>
+            </li>
+            <li>
+              <label>
+                Food and Drink{" "}
+                <input
+                  type="checkbox"
+                  value={food}
+                  onChange={(e) => setFood((checked) => !checked)}
+                  checked={food}
+                />
+              </label>
+            </li>
+            <li>
+              <label>
+                Toilets{" "}
+                <input
+                  type="checkbox"
+                  value={toilets}
+                  onChange={(e) => setToilets((checked) => !checked)}
+                  checked={toilets}
+                />
+              </label>
+            </li>
+          </ul>
 
-        {/* MAP */}
+          <div className="addlocation__map-container">
+            {/* <Map lng={lng} setLng={setLng} lat={lat} setLat={setLat} /> */}
+          </div>
 
-        <button className="submit-btn">Submit</button>
-      </form>
+          <button className="submit-btn" disabled={isLoading}>
+            Submit
+          </button>
+        </form>
+      )}
     </div>
   );
 };
