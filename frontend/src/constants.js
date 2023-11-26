0de947b2195473f38e94fc8108cf4a6ba148930b
Jake Samuels <samuels242@googlemail.com>
2023-11-26 19:02:13 +0000

    Fix base url

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,3 +1,6 @@
-export const BASE_URL = "";
+export const BASE_URL =
+  process.env.NODE_ENV === "development" ? "http://localhost:8000" : "";
+
+// export const BASE_URL = "";
 export const LOCATIONS_URL = "/api/locations";
 export const USERS_URL = "/api/users";
