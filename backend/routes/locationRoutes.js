b0a916897b6f578322630c581df9cf83a1eb11d4
Jake Samuels <samuels242@googlemail.com>
2023-11-26 10:25:00 +0000

    Remove favourite

--- backend/routes/locationRoutes.js
+++ backend/routes/locationRoutes.js
@@ -10,12 +10,16 @@ import {
   getSavedLocations,
   getFavouriteLocations,
   deleteLocation,
+  removeFavourite,
 } from "./../controllers/locationController.js";
 
 router.route("/saved").get(protect, getSavedLocations);
 router.route("/favourites").get(protect, getFavouriteLocations);
 router.route("/").get(getLocations).post(protect, addLocation);
 router.route("/:id").get(getLocationById).delete(protect, deleteLocation);
-router.route("/:id/favourites").post(protect, addFavourite);
+router
+  .route("/:id/favourites")
+  .post(protect, addFavourite)
+  .delete(protect, removeFavourite);
 
 export default router;
