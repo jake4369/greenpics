9d462ce5820f991bf7cccdce4e32bdc946bb1c80
Jake Samuels <samuels242@googlemail.com>
2023-11-26 00:54:08 +0000

    Render controls based on location pathname

--- frontend/src/components/Shared/Map.jsx
+++ frontend/src/components/Shared/Map.jsx
@@ -1,10 +1,12 @@
 import React, { useRef, useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import mapboxgl from "mapbox-gl";
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoiamRvZ2c0MzY5IiwiYSI6ImNscGJjdWlxdTBkY3IycW82MnJhaDBsN3gifQ.L4KHq_l8UVwmgHm6GagcrA";
 
-const Map = ({ lng, setLng, lat, setLat }) => {
+const Map = ({ lng, setLng, lat, setLat, customZoom }) => {
+  const { pathname } = useLocation();
   const mapContainerRef = useRef(null);
 
   const [zoom, setZoom] = useState(4.08);
@@ -14,17 +16,19 @@ const Map = ({ lng, setLng, lat, setLat }) => {
       container: mapContainerRef.current,
       style: "mapbox://styles/mapbox/streets-v11",
       center: [lng, lat],
-      zoom: zoom,
+      zoom: customZoom || zoom,
     });
 
     // Add navigation control (the +/- zoom buttons)
-    map.addControl(new mapboxgl.NavigationControl(), "top-right");
+    if (pathname === "/dashboard/addlocation") {
+      map.addControl(new mapboxgl.NavigationControl(), "top-right");
 
-    map.on("move", () => {
-      setLng(map.getCenter().lng.toFixed(4));
-      setLat(map.getCenter().lat.toFixed(4));
-      setZoom(map.getZoom().toFixed(2));
-    });
+      map.on("move", () => {
+        setLng(map.getCenter().lng.toFixed(4));
+        setLat(map.getCenter().lat.toFixed(4));
+        setZoom(map.getZoom().toFixed(2));
+      });
+    }
 
     // Clean up on unmount
     return () => map.remove();
@@ -33,7 +37,7 @@ const Map = ({ lng, setLng, lat, setLat }) => {
   return (
     <div>
       <div className="map-sidebar">
-        Longitude: {lng} | Latitude: {lat}
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
       </div>
       <div ref={mapContainerRef} className="map-container" />
     </div>
