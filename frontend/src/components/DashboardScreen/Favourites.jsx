f5d19e8e3097b0e304b3e2301d9fddea7eddc406
Jake Samuels <samuels242@googlemail.com>
2023-11-26 10:28:50 +0000

    Favourites screen

---
+++ frontend/src/components/DashboardScreen/Favourites.jsx
@@ -0,0 +1,43 @@
+import { useGetFavouriteLocationsQuery } from "./../../slices/locationsSlice";
+
+import Card from "./../../Components/Shared/Card";
+import Loader from "./../../Components/Shared/Loader";
+
+const Favourites = () => {
+  const {
+    data: locations,
+    isLoading,
+    isError,
+    refetch,
+  } = useGetFavouriteLocationsQuery();
+
+  const cards = !isLoading
+    ? locations.map((location) => (
+        <Card key={location._id} location={location} refetch={refetch} />
+      ))
+    : [];
+
+  return (
+    <div className="favourites-screen">
+      <h1>My Favourites</h1>
+
+      <div className="favourites-screen__card-grid">
+        {isLoading ? (
+          <Loader />
+        ) : cards.length ? (
+          isError ? (
+            <p>Error...</p>
+          ) : (
+            cards
+          )
+        ) : (
+          <p className="no-data__message">
+            You haven't added any locations to your favourites
+          </p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Favourites;
