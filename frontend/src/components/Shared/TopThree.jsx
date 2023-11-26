aa995b420a27a1979a9fa2f8d57fb79e05828909
Jake Samuels <samuels242@googlemail.com>
2023-11-26 15:43:16 +0000

    Fix conflicts

--- frontend/src/components/Shared/TopThree.jsx
+++ frontend/src/components/Shared/TopThree.jsx
@@ -4,24 +4,17 @@ import Card from "./Card";
 const TopThree = () => {
   const { data: locations, isLoading, isError } = useGetLocationsQuery();
 
-
-
   const sortedLocations = !isLoading
-    ? locations.slice(0,3).sort((a, b) => b.numReviews - a.numReviews)
+    ? locations.slice(0, 3).sort((a, b) => b.numReviews - a.numReviews)
     : [];
-  
 
   return (
-
-    sortedLocations.map(location => (<Card key={location._id} location={location}/>))
-
-)}
-
-
-  console.log(locations);
-
-  return <div>Top Three</div>;
+    <div>
+      {sortedLocations.map((location) => (
+        <Card key={location._id} location={location} />
+      ))}
+    </div>
+  );
 };
-
 
 export default TopThree;
