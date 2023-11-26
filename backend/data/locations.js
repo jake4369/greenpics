2b4af0f23e02671b6e2770e082fba1c22468f9ad
Jake Samuels <samuels242@googlemail.com>
2023-11-26 08:10:29 +0000

    Edit dev data

--- backend/data/locations.js
+++ backend/data/locations.js
@@ -3,13 +3,11 @@ const locations = [
     name: "Sutton Park National Nature Reserve",
     username: "jake123",
     county: "West Midlands",
-    lng: "52.609024",
-    lat: "-2.0840448",
-    numReviews: 10,
-    rating: 4.5,
-    img: [
-      "https://eu-assets.simpleview-europe.com/birmingham-visit/imageresizer/?image=%2Fdmsimgs%2FSutton_Park_85739525.jpg&action=ProductDetailProFullWidth",
-    ],
+    lat: "52.609024",
+    long: "-2.0840448",
+    numReviews: 0,
+    img: "https://eu-assets.simpleview-europe.com/birmingham-visit/imageresizer/?image=%2Fdmsimgs%2FSutton_Park_85739525.jpg&action=ProductDetailProFullWidth",
+
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis odit cumque autem excepturi est veritatis suscipit, enim id a expedita possimus, hic ab officia fuga accusantium labore perspiciatis culpa, qui esse rem ea eveniet quas modi praesentium! Quasi alias blanditiis obcaecati possimus. Nisi, dolorum veniam exercitationem placeat consectetur perspiciatis at.",
     parking: true,
@@ -22,13 +20,11 @@ const locations = [
     name: "Malvern Hills",
     username: "jake123",
     county: "West Midlands",
-    lng: "52.1674966",
-    lat: "-2.7356114",
-    numReviews: 4,
-    rating: 3.5,
-    img: [
-      "https://www.visitthemalverns.org/wp-content/uploads/2021/06/British-Camp-1920-1200x899.jpg",
-    ],
+    lng: "-2.7356114",
+    lat: "52.1674966",
+    numReviews: 0,
+    img: "https://www.visitthemalverns.org/wp-content/uploads/2021/06/British-Camp-1920-1200x899.jpg",
+
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis odit cumque autem excepturi est veritatis suscipit, enim id a expedita possimus, hic ab officia fuga accusantium labore perspiciatis culpa, qui esse rem ea eveniet quas modi praesentium! Quasi alias blanditiis obcaecati possimus. Nisi, dolorum veniam exercitationem placeat consectetur perspiciatis at.",
     parking: true,
@@ -41,13 +37,11 @@ const locations = [
     name: "Lickey Hills Country Park",
     username: "jake123",
     county: "West Midlands",
-    lng: "52.3723358",
-    lat: "-2.0093782",
-    numReviews: 13,
-    rating: 4.8,
-    img: [
-      "https://letsgowiththechildren.co.uk/wp-content/uploads/2016/12/Lickey-Hills-Country-Park-1-915px.jpg",
-    ],
+    lat: "52.3723358",
+    lng: "-2.0093782",
+    numReviews: 0,
+    img: "https://letsgowiththechildren.co.uk/wp-content/uploads/2016/12/Lickey-Hills-Country-Park-1-915px.jpg",
+
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis odit cumque autem excepturi est veritatis suscipit, enim id a expedita possimus, hic ab officia fuga accusantium labore perspiciatis culpa, qui esse rem ea eveniet quas modi praesentium! Quasi alias blanditiis obcaecati possimus. Nisi, dolorum veniam exercitationem placeat consectetur perspiciatis at.",
     parking: false,
@@ -60,13 +54,10 @@ const locations = [
     name: "Chiltern Hills",
     username: "jake123",
     county: "Buckinghamshire",
-    lng: "51.7250907",
-    lat: "-0.8195256",
-    numReviews: 37,
-    rating: 4.9,
-    img: [
-      "https://cdn.britannica.com/08/144908-050-3F03692F/Chiltern-Hills-England.jpg",
-    ],
+    lat: "51.7250907",
+    lng: "-0.8195256",
+    numReviews: 0,
+    img: "https://cdn.britannica.com/08/144908-050-3F03692F/Chiltern-Hills-England.jpg",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis odit cumque autem excepturi est veritatis suscipit, enim id a expedita possimus, hic ab officia fuga accusantium labore perspiciatis culpa, qui esse rem ea eveniet quas modi praesentium! Quasi alias blanditiis obcaecati possimus. Nisi, dolorum veniam exercitationem placeat consectetur perspiciatis at.",
     parking: true,
@@ -79,13 +70,11 @@ const locations = [
     name: "Aysgarth Falls",
     username: "jake123",
     county: "Yorkshire",
-    lng: "54.2945501",
-    lat: "-1.985987",
-    numReviews: 84,
-    rating: 4.3,
-    img: [
-      "https://upload.wikimedia.org/wikipedia/commons/e/e6/Aysgarth_High_Force.JPG",
-    ],
+    lat: "54.2945501",
+    lng: "-1.985987",
+    numReviews: 0,
+    img: "https://upload.wikimedia.org/wikipedia/commons/e/e6/Aysgarth_High_Force.JPG",
+
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis odit cumque autem excepturi est veritatis suscipit, enim id a expedita possimus, hic ab officia fuga accusantium labore perspiciatis culpa, qui esse rem ea eveniet quas modi praesentium! Quasi alias blanditiis obcaecati possimus. Nisi, dolorum veniam exercitationem placeat consectetur perspiciatis at.",
     parking: true,
@@ -98,13 +87,11 @@ const locations = [
     name: "Malham Cove",
     username: "jake123",
     county: "Yorkshire",
-    lng: "54.0728293",
-    lat: "-2.1605043",
-    numReviews: 17,
-    rating: 2.5,
-    img: [
-      "https://upload.wikimedia.org/wikipedia/commons/7/7b/Malham_Cove.jpg",
-    ],
+    lat: "54.0728293",
+    lng: "-2.1605043",
+    numReviews: 0,
+    img: "https://upload.wikimedia.org/wikipedia/commons/7/7b/Malham_Cove.jpg",
+
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis odit cumque autem excepturi est veritatis suscipit, enim id a expedita possimus, hic ab officia fuga accusantium labore perspiciatis culpa, qui esse rem ea eveniet quas modi praesentium! Quasi alias blanditiis obcaecati possimus. Nisi, dolorum veniam exercitationem placeat consectetur perspiciatis at.",
     parking: false,
@@ -117,13 +104,11 @@ const locations = [
     name: "Jurassic Coast",
     username: "jake123",
     county: "Devon",
-    lng: "50.6161608",
-    lat: "-3.9203241",
-    numReviews: 130,
-    rating: 4.8,
-    img: [
-      "https://lp-cms-production.imgix.net/2022-07/england-GettyImages-521766722-rfc.jpeg?auto=format&q=75&w=1920",
-    ],
+    lat: "50.6161608",
+    lng: "-3.9203241",
+    numReviews: 0,
+    img: "https://lp-cms-production.imgix.net/2022-07/england-GettyImages-521766722-rfc.jpeg?auto=format&q=75&w=1920",
+
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis odit cumque autem excepturi est veritatis suscipit, enim id a expedita possimus, hic ab officia fuga accusantium labore perspiciatis culpa, qui esse rem ea eveniet quas modi praesentium! Quasi alias blanditiis obcaecati possimus. Nisi, dolorum veniam exercitationem placeat consectetur perspiciatis at.",
     parking: false,
@@ -136,13 +121,11 @@ const locations = [
     name: "St. Nectan's Glen",
     username: "jake123",
     county: "Cornwall",
-    lng: "50.6649851",
-    lat: "-4.718956",
-    numReviews: 110,
-    rating: 5,
-    img: [
-      "https://www.st-nectansglen.co.uk/wp-content/uploads/2020/08/waterfall-one.jpg",
-    ],
+    lat: "50.6649851",
+    lng: "-4.718956",
+    numReviews: 0,
+    img: "https://www.st-nectansglen.co.uk/wp-content/uploads/2020/08/waterfall-one.jpg",
+
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis odit cumque autem excepturi est veritatis suscipit, enim id a expedita possimus, hic ab officia fuga accusantium labore perspiciatis culpa, qui esse rem ea eveniet quas modi praesentium! Quasi alias blanditiis obcaecati possimus. Nisi, dolorum veniam exercitationem placeat consectetur perspiciatis at.",
     parking: true,
@@ -155,13 +138,11 @@ const locations = [
     name: "Coquet Valley",
     username: "jake123",
     county: "Northumberland",
-    lng: "55.3409312",
-    lat: "-2.4102884",
-    numReviews: 7,
-    rating: 4.1,
-    img: [
-      "https://www.visitnorthumberland.com/VisitNorthumberland/media/VisitNorthumberland/Home/Explore/Destinations/Scenic%20Spots/Coquet%20Valley/coquet-valley-2023.jpg?ext=.jpg",
-    ],
+    lat: "55.3409312",
+    lng: "-2.4102884",
+    numReviews: 0,
+    img: "https://www.visitnorthumberland.com/VisitNorthumberland/media/VisitNorthumberland/Home/Explore/Destinations/Scenic%20Spots/Coquet%20Valley/coquet-valley-2023.jpg?ext=.jpg",
+
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis odit cumque autem excepturi est veritatis suscipit, enim id a expedita possimus, hic ab officia fuga accusantium labore perspiciatis culpa, qui esse rem ea eveniet quas modi praesentium! Quasi alias blanditiis obcaecati possimus. Nisi, dolorum veniam exercitationem placeat consectetur perspiciatis at.",
     parking: false,
@@ -176,11 +157,9 @@ const locations = [
     county: "Kent",
     lng: "51.2754252",
     lat: "1.0592147",
-    numReviews: 57,
-    rating: 4.6,
-    img: [
-      "https://seekent.b-cdn.net/wp-content/uploads/2020/10/Canterbury-Hambrook-Marshes-CI.jpg",
-    ],
+    numReviews: 0,
+    img: "https://seekent.b-cdn.net/wp-content/uploads/2020/10/Canterbury-Hambrook-Marshes-CI.jpg",
+
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis odit cumque autem excepturi est veritatis suscipit, enim id a expedita possimus, hic ab officia fuga accusantium labore perspiciatis culpa, qui esse rem ea eveniet quas modi praesentium! Quasi alias blanditiis obcaecati possimus. Nisi, dolorum veniam exercitationem placeat consectetur perspiciatis at.",
     parking: true,
