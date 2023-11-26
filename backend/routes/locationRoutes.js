c60046199ed0c8e343b9a140cdb7acab5de3d3b0
Jake Samuels <samuels242@googlemail.com>
2023-11-26 09:12:11 +0000

    Add save to favourites routes

--- backend/routes/locationRoutes.js
+++ backend/routes/locationRoutes.js
@@ -6,12 +6,16 @@ import {
   getLocations,
   getLocationById,
   addLocation,
+  addFavourite,
   getSavedLocations,
+  getFavouriteLocations,
   deleteLocation,
 } from "./../controllers/locationController.js";
 
 router.route("/saved").get(protect, getSavedLocations);
+router.route("/favourites").get(protect, getFavouriteLocations);
 router.route("/").get(getLocations).post(protect, addLocation);
 router.route("/:id").get(getLocationById).delete(protect, deleteLocation);
+router.route("/:id/favourites").post(protect, addFavourite);
 
 export default router;
