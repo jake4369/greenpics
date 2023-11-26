b96a9bc68223f91c0a330a29657057e09769947f
Jake Samuels <samuels242@googlemail.com>
2023-11-26 07:32:25 +0000

    Change app name

--- frontend/src/components/HomeScreen/Hero.jsx
+++ frontend/src/components/HomeScreen/Hero.jsx
@@ -2,10 +2,14 @@ const Hero = () => {
   return (
     <div className="hero">
       <div className="hero__text-content">
-        <h1>GreenPics</h1>
-        <p>GreenPics is a place to find and share pictures of beautiful, natural, or green places.
-        <br/>
-        Whether you're looking for somewhere green to visit, or want to share your favourite green places from lush forests to vibrant gardens, explore an array of breathtaking green landscapes.
+        <h1>GreenSpace</h1>
+        <p>
+          GreenSpace is a place to find and share pictures of beautiful,
+          natural, or green places.
+          <br />
+          Whether you're looking for somewhere green to visit, or want to share
+          your favourite green places from lush forests to vibrant gardens,
+          explore an array of breathtaking green landscapes.
         </p>
       </div>
     </div>
