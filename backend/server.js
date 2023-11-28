9b1de2dccda1e377d200457793e55e93a64bbe09
Jake Samuels <samuels242@googlemail.com>
2023-11-28 12:32:04 +0000

    Fix deployment issues

--- backend/server.js
+++ backend/server.js
@@ -26,22 +26,33 @@ app.use("/api/locations", locationRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/upload", uploadRoutes);
 
+// if (process.env.NODE_ENV === "production") {
+//   const __dirname = path.resolve();
+//   app.use("/uploads", express.static("/var/data/uploads"));
+//   app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+//   );
+// } else {
+//   const __dirname = path.resolve();
+//   app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+//   app.get("/", (req, res) => {
+//     res.send("API is running....");
+//   });
+// }
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
 if (process.env.NODE_ENV === "production") {
-  const __dirname = path.resolve();
-  app.use(
-    "/uploads",
-    express.static(path.join(__dirname, "/var/data/uploads"))
-  );
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "/frontend/build"))
   );
 } else {
-  const __dirname = path.resolve();
-  app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
   app.get("/", (req, res) => {
-    res.send("API is running....");
+    res.send("API is running...");
   });
 }
 
