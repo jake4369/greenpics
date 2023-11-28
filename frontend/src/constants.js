44a069811c42f06ae83b224bff1f4e15ec7a4ed9
Jake Samuels <samuels242@googlemail.com>
2023-11-28 10:38:59 +0000

    Add base url

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,6 @@
 // export const BASE_URL =
 //   process.env.NODE_ENV === "development" ? "http://localhost:8000" : "";
 
-export const BASE_URL = "https://greenspace.onrender.com";
+export const BASE_URL = "";
 export const LOCATIONS_URL = "/api/locations";
 export const USERS_URL = "/api/users";
