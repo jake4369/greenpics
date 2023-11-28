664817159f4680369065eb0ad3db7b5fd67544e3
Jake Samuels <samuels242@googlemail.com>
2023-11-28 10:42:03 +0000

    server.js

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,6 @@ app.use(express.urlencoded({ extended: true }));
 // Cookie parser middleware
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
 app.use("/api/locations", locationRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/upload", uploadRoutes);
